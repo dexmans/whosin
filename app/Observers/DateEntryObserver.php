b6a36527d112f41165c896167d84d6e9f47a3512
Robert van Geerenstein <info@r15n.nl>
2017-07-05 20:11:52 +0200

    Fixing observer to allow user_id to be set when in CLI

--- app/Observers/DateEntryObserver.php
+++ app/Observers/DateEntryObserver.php
@@ -2,6 +2,7 @@
 
 namespace App\Observers;
 
+use Exception;
 use App\Models\DateEntry;
 use TID\Observers\BaseObserver;
 
@@ -9,6 +10,13 @@ class DateEntryObserver extends BaseObserver
 {
     public function creating(DateEntry $entry)
     {
+        if (app()->runningInConsole()) {
+            if (! $entry->user_id) {
+                throw new \Exception('When in CLI, make sure you set the user');
+            }
+            return true;
+        }
+
         if (! auth()->check()) {
             return false;
         }
